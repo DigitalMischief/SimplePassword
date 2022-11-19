f2791d3495ba9f82f3ec7a5ad8d79b169dcf59f5
Josh Kane <jgkane2119@yahoo.com>
2022-11-19 11:16:34 -0800

    establishes directory path and creates config file in it

--- src/main/java/com/llamalabb/simplepassword/simplepassword/ConfigManager.kt
+++ src/main/java/com/llamalabb/simplepassword/simplepassword/ConfigManager.kt
@@ -7,18 +7,21 @@ import java.io.OutputStreamWriter
 import java.util.Properties
 
 object ConfigManager {
-    private const val BASE_FILE_PATH = "./SimplePassword"
-    private const val CONFIG_FILE_PATH = "$BASE_FILE_PATH/config.properties"
+    private val runTimePath =
+        (ConfigManager.javaClass.protectionDomain.codeSource.location.toURI()).path + "/SimplePassword"
+    private const val CONFIG_FILE_PATH = "config.properties"
     private const val PASSWORD_KEY = "password"
     private const val DEFAULT_PASSWORD = "serverpw"
 
     private val config by lazy { initializeConfig() }
 
-    private fun initializeConfig() : Properties {
+    private fun initializeConfig(): Properties {
         return if (File(CONFIG_FILE_PATH).exists()) {
             Properties().apply { load(FileInputStream(CONFIG_FILE_PATH)) }
         } else {
-            val file = File(CONFIG_FILE_PATH).apply { createNewFile() }
+            File(runTimePath).mkdirs()
+            val file = File(runTimePath, CONFIG_FILE_PATH).apply { createNewFile() }
+            println(file.absolutePath)
             Properties().apply {
                 setProperty(PASSWORD_KEY, DEFAULT_PASSWORD)
                 store(OutputStreamWriter(FileOutputStream(file)), "")
